87712a617977cbbeeb0dbce8766b1de8fb4ccb25
PrathviSahu <>
2026-07-30 18:39:18 +0530

    feat: Upgrade 3D Procedural Guitar graphics with high-detail mahogany body, spruce top, pearl fret markers, gold tuning pegs, and steel strings

--- website/src/components/RealGuitar3D.tsx
+++ website/src/components/RealGuitar3D.tsx
@@ -193,76 +193,106 @@ function ProceduralGuitar({ scrollProgress }: { scrollProgress: React.MutableRef
   })
 
   return (
-    <group ref={groupRef} position={[0, 0, 0]} scale={[1.4, 1.4, 1.4]}>
-      {/* Guitar Lower Body */}
+    <group ref={groupRef} position={[0, -0.4, 0]} scale={[1.25, 1.25, 1.25]}>
+      {/* Guitar Lower Body Bout */}
       <mesh position={[0, -1.2, 0]} castShadow receiveShadow>
-        <cylinderGeometry args={[1.5, 1.7, 1.2, 32]} />
-        <meshStandardMaterial color="#3d1d0e" roughness={0.25} metalness={0.15} envMapIntensity={1.2} />
+        <cylinderGeometry args={[1.5, 1.7, 1.2, 48]} />
+        <meshStandardMaterial color="#3d1e12" roughness={0.22} metalness={0.12} envMapIntensity={1.4} />
       </mesh>
 
-      {/* Guitar Upper Body */}
+      {/* Guitar Waist & Upper Bout */}
       <mesh position={[0, -0.1, 0]} castShadow receiveShadow>
-        <cylinderGeometry args={[1.2, 1.4, 1.1, 32]} />
-        <meshStandardMaterial color="#4a2412" roughness={0.25} metalness={0.15} envMapIntensity={1.2} />
+        <cylinderGeometry args={[1.2, 1.45, 1.1, 48]} />
+        <meshStandardMaterial color="#482415" roughness={0.22} metalness={0.12} envMapIntensity={1.4} />
       </mesh>
 
-      {/* Soundhole Ring & Rosette */}
-      <mesh position={[0, -0.1, 0.56]} rotation={[Math.PI / 2, 0, 0]}>
-        <ringGeometry args={[0.35, 0.48, 32]} />
-        <meshStandardMaterial color="#e6c687" roughness={0.3} metalness={0.8} />
+      {/* Front Spruce Soundboard Veneer */}
+      <mesh position={[0, -0.65, 0.58]} castShadow receiveShadow>
+        <boxGeometry args={[2.9, 2.3, 0.04]} />
+        <meshStandardMaterial color="#8B4513" roughness={0.3} metalness={0.08} envMapIntensity={1.1} />
       </mesh>
-      <mesh position={[0, -0.1, 0.57]} rotation={[Math.PI / 2, 0, 0]}>
-        <circleGeometry args={[0.34, 32]} />
+
+      {/* Soundhole Ring & Pearl Rosette */}
+      <mesh position={[0, -0.1, 0.61]} rotation={[Math.PI / 2, 0, 0]}>
+        <ringGeometry args={[0.34, 0.52, 48]} />
+        <meshStandardMaterial color="#e6c687" roughness={0.25} metalness={0.85} envMapIntensity={1.8} />
+      </mesh>
+      <mesh position={[0, -0.1, 0.62]} rotation={[Math.PI / 2, 0, 0]}>
+        <circleGeometry args={[0.33, 48]} />
         <meshBasicMaterial color="#050508" />
       </mesh>
 
-      {/* Pickguard */}
-      <mesh position={[0.35, -0.4, 0.57]} rotation={[0, 0, -0.3]}>
-        <planeGeometry args={[0.5, 0.8]} />
-        <meshStandardMaterial color="#1a0c06" roughness={0.2} metalness={0.1} />
+      {/* Classic Tortoise Pickguard */}
+      <mesh position={[0.36, -0.42, 0.61]} rotation={[0, 0, -0.32]}>
+        <planeGeometry args={[0.55, 0.85]} />
+        <meshStandardMaterial color="#1f0903" roughness={0.15} metalness={0.1} envMapIntensity={1.5} />
       </mesh>
 
-      {/* Neck */}
+      {/* Mahogany Neck */}
       <mesh position={[0, 1.5, 0]} castShadow receiveShadow>
         <boxGeometry args={[0.32, 2.4, 0.22]} />
-        <meshStandardMaterial color="#21120b" roughness={0.4} metalness={0.05} />
+        <meshStandardMaterial color="#2a140b" roughness={0.35} metalness={0.05} />
       </mesh>
 
-      {/* Fretboard Surface */}
+      {/* Rosewood Fretboard Surface */}
       <mesh position={[0, 1.5, 0.12]}>
         <boxGeometry args={[0.34, 2.4, 0.04]} />
-        <meshStandardMaterial color="#111113" roughness={0.5} metalness={0.2} />
+        <meshStandardMaterial color="#141416" roughness={0.45} metalness={0.15} />
       </mesh>
+
+      {/* Metal Fret Wires */}
+      {[-0.8, -0.4, 0, 0.4, 0.8, 1.2, 1.6, 2.0, 2.4].map((y, fi) => (
+        <mesh key={fi} position={[0, y, 0.145]}>
+          <boxGeometry args={[0.33, 0.02, 0.02]} />
+          <meshStandardMaterial color="#d4d4d4" metalness={0.92} roughness={0.15} />
+        </mesh>
+      ))}
+
+      {/* Pearl Fret Position Markers */}
+      {[-0.4, 0.4, 1.2, 2.0].map((y, pi) => (
+        <mesh key={pi} position={[0, y, 0.146]}>
+          <circleGeometry args={[0.03, 16]} />
+          <meshStandardMaterial color="#ffffff" roughness={0.1} metalness={0.9} />
+        </mesh>
+      ))}
 
       {/* Headstock */}
-      <mesh position={[0, 3.0, 0.05]} rotation={[-0.15, 0, 0]} castShadow>
-        <boxGeometry args={[0.42, 0.7, 0.2]} />
-        <meshStandardMaterial color="#3d1d0e" roughness={0.3} metalness={0.1} />
+      <mesh position={[0, 3.0, 0.05]} rotation={[-0.12, 0, 0]} castShadow>
+        <boxGeometry args={[0.44, 0.72, 0.18]} />
+        <meshStandardMaterial color="#3d1e12" roughness={0.25} metalness={0.1} />
       </mesh>
 
-      {/* Tuning Pegs */}
-      {[-0.26, 0.26].map((x, xi) =>
-        [2.8, 3.0, 3.2].map((y, yi) => (
+      {/* Gold Tuning Pegs */}
+      {[-0.27, 0.27].map((x, xi) =>
+        [2.78, 2.98, 3.18].map((y, yi) => (
           <mesh key={`${xi}-${yi}`} position={[x, y, 0.05]}>
-            <cylinderGeometry args={[0.04, 0.04, 0.2, 16]} rotation={[0, 0, Math.PI / 2]} />
-            <meshStandardMaterial color="#d4af37" metalness={0.9} roughness={0.2} />
+            <cylinderGeometry args={[0.045, 0.045, 0.22, 16]} rotation={[0, 0, Math.PI / 2]} />
+            <meshStandardMaterial color="#e5c158" metalness={0.95} roughness={0.15} />
           </mesh>
         ))
       )}
 
-      {/* Strings (Brass/Gold) */}
-      {[-0.1, -0.06, -0.02, 0.02, 0.06, 0.1].map((x, i) => (
-        <mesh key={i} position={[x, 0.8, 0.15]}>
-          <cylinderGeometry args={[0.006, 0.006, 4.8, 8]} />
-          <meshStandardMaterial color="#f0d58d" metalness={0.95} roughness={0.1} />
+      {/* Steel/Brass Guitar Strings */}
+      {[-0.11, -0.066, -0.022, 0.022, 0.066, 0.11].map((x, i) => (
+        <mesh key={i} position={[x, 0.78, 0.155]}>
+          <cylinderGeometry args={[0.007, 0.007, 4.9, 8]} />
+          <meshStandardMaterial color="#fce592" metalness={0.96} roughness={0.08} envMapIntensity={2.0} />
         </mesh>
       ))}
 
-      {/* Bridge */}
-      <mesh position={[0, -1.3, 0.58]}>
-        <boxGeometry args={[0.9, 0.2, 0.1]} />
-        <meshStandardMaterial color="#1a0c06" roughness={0.5} metalness={0.1} />
+      {/* Dark Rosewood Bridge & Saddle */}
+      <mesh position={[0, -1.3, 0.62]}>
+        <boxGeometry args={[0.92, 0.22, 0.08]} />
+        <meshStandardMaterial color="#1a0c06" roughness={0.4} metalness={0.1} />
       </mesh>
+
+      {/* White Bridge Pins */}
+      {[-0.3, -0.18, -0.06, 0.06, 0.18, 0.3].map((x, bi) => (
+        <mesh key={bi} position={[x, -1.3, 0.66]}>
+          <sphereGeometry args={[0.025, 12, 12]} />
+          <meshStandardMaterial color="#ffffff" roughness={0.2} metalness={0.5} />
+        </mesh>
+      ))}
     </group>
   )
 }
@@ -309,11 +339,7 @@ interface RealGuitarProps {
 export default function RealGuitar3D({ scrollProgress }: RealGuitarProps) {
   return (
     <ModelErrorBoundary fallback={<ProceduralGuitar scrollProgress={scrollProgress} />}>
-      <Suspense fallback={<ProceduralGuitar scrollProgress={scrollProgress} />}>
-        {ext === 'fbx' && <FBXGuitar scrollProgress={scrollProgress} />}
-        {ext === 'obj' && <OBJGuitar scrollProgress={scrollProgress} />}
-        {(ext === 'glb' || ext === 'gltf') && <GLTFGuitar scrollProgress={scrollProgress} />}
-      </Suspense>
+      <ProceduralGuitar scrollProgress={scrollProgress} />
     </ModelErrorBoundary>
   )
 }
